6c0d5c0cc1064b0838b359bb42ca294a15a34d0b
ronzuma <ronzuma@gmail.com>
2024-04-30 10:11:42 +0800

    patching menu site

--- application/controllers/Site.php
+++ application/controllers/Site.php
@@ -11,17 +11,31 @@ class Site extends MY_Controller
 	}
 	
 	public function index(){
-		$ap_id_user = $this->session->userdata('ap_id_user');
-		$site=$this->M_site->site($ap_id_user);
-		$kota=json_decode($this->M_site->kota($site->provinsi));
-		$data['kota']=$kota->result;
-		$kecamatan=json_decode($this->M_site->kecamatan($site->kabupaten));
-		$data['kecamatan']=$kecamatan->result;
-		$kelurahan=json_decode($this->M_site->kelurahan($site->kecamatan));
-		$data['kelurahan']=$kelurahan->result;
-		$data['site']=$site;
-		$data['provinsi']=$this->M_site->provinsi();
+		$ap_id_user 	= $this->session->userdata('ap_id_user');
+		$data['site']	= $this->M_site->site($ap_id_user);
 		$this->load->view('site/index', $data);
+	}
+
+	public function detail(){
+		$idsite  = $this->input->get('id');
+		$ms_regions_id  = $this->input->get('region');
+
+		if($idsite==null){
+			$data['provinsi']=$this->M_site->provinsi();
+			$data['ms_regions_id']=$ms_regions_id;
+			$this->load->view('site/add', $data);
+		}else{
+			$site=$this->M_site->detailsite($idsite);
+			$kota=json_decode($this->M_site->kota($site->provinsi));
+			$data['kota']=$kota->result;
+			$kecamatan=json_decode($this->M_site->kecamatan($site->kabupaten));
+			$data['kecamatan']=$kecamatan->result;
+			$kelurahan=json_decode($this->M_site->kelurahan($site->kecamatan));
+			$data['kelurahan']=$kelurahan->result;
+			$data['site']=$site;
+			$data['provinsi']=$this->M_site->provinsi();
+			$this->load->view('site/detail', $data);
+		}
 	}
 
 	public function kota()
@@ -42,9 +56,9 @@ class Site extends MY_Controller
 		echo $this->M_site->kelurahan($kode);
 	}
 
-	public function edit_proses(){
+	public function add_proses(){
 		$body=array(
-			'id'					=> '2',
+			'ms_regions_id'			=> $this->input->post('ms_regions_id'),
 			'site_name' 			=> $this->input->post('site_name'),
 			'desa' 					=> $this->input->post('kelurahan'),
 			'kecamatan'				=> $this->input->post('kecamatan'),
@@ -62,8 +76,30 @@ class Site extends MY_Controller
 		);
 
 		$status=$this->db->insert('ms_sites', $body);
-		var_dump($status);
-		//redirect('Site');
+		redirect('Site');
 	}
 
+	public function edit_proses(){
+		$id=$this->input->post('idsite');
+		$body=array(
+			'site_name' 			=> $this->input->post('site_name'),
+			'desa' 					=> $this->input->post('kelurahan'),
+			'kecamatan'				=> $this->input->post('kecamatan'),
+			'kabupaten'				=> $this->input->post('kota'),
+			'provinsi'				=> $this->input->post('provinsi'),
+			'elev_tanggul_utama'	=> $this->input->post('elev_tanggul_utama'),
+			'elev_tanggul_pembantu'	=> $this->input->post('elev_tanggul_pembantu'),
+			'elev_pelimpah'			=> $this->input->post('elev_pelimpah'),
+			'elev_pelimpah_pembantu'=> $this->input->post('elev_pelimpah_pembantu'),
+			'elev_normal'			=> $this->input->post('elev_normal'),
+			'elev_siaga3'			=> $this->input->post('elev_siaga3'),
+			'elev_siaga2'			=> $this->input->post('elev_siaga2'),
+			'elev_siaga1'			=> $this->input->post('elev_siaga1'),
+			'batas_kritis_vwp'		=> $this->input->post('batas_kritis_vwp')
+		);
+
+		$this->db->where('id', $id);
+		$this->db->update('ms_sites', $body);
+		redirect('Site');
+	}
 }
